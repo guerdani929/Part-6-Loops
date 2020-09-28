b2d07ec8a79ab0f973dab65ee54216158f15b437
guerdani929 <GuerDani929@GoTVDSB.ca>
2020-09-28 08:33:20 -0400

    Add project files.

---
+++ Part 6 Loops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_6_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello! ");
+            //}
+
+            int i = 10;
+            while (i <= 1)
+            {
+                Console.WriteLine(i);
+                i = i - 1;
+
+
+
+
+                Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
